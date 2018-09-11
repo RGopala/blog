da48d28c2e79c9ab832e4adde46163871d36c859
Gopal Tamang <gopalroyal831@gmail.com>
2018-09-11 21:45:03 +0545

    Initial commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div class="line">
+         <footer>
+            <div class="s-12 l-8">
+               <p>
+                  Copyright 2018, Vision Design - graphic zoo<br>
+                  All images is purchased from Bigstock. Do not use the image in your website.
+               </p>
+            </div>
+            <div class="s-12 l-4">		                            
+               <a class="right" href="#" title="Responsive framework">Design & Developed <br>
+               by Cherry Inc.
+               </a>                       
+            </div>
+         </footer>
+      </div>
+   </body>
+</html>
---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width" />
+      <link rel="icon" href="img/cherry-inc.jpg" sizes="16x16" type="image/png">
+      <title>Cherry Blog | Everything you need to Know!
+      </title>
+      <meta name="description" content="Cherry Blog is a blog that that has everything in it. Blog that makes you smarter everyday. Innovative, smart tips and tricks along with the basic details in your blog."/>
+      <meta name="keywords" content="free blog nepal, blog in nepal, blogger in Nepal, Cherry Blog Nepal"/>
+      <link rel="stylesheet" href="css/components.css">
+      <link rel="stylesheet" href="css/responsee.css">
+      <!-- CUSTOM STYLE -->       
+      <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" href="css/template-style.css">
+      <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+      <script type="text/javascript" src="js/jquery-ui.min.js"></script>    
+      <script type="text/javascript" src="js/modernizr.js"></script>
+      <script type="text/javascript" src="js/responsee.js"></script>          
+      
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120216118-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-120216118-2');
+</script>
+
+  
+   </head>
+   <body class="size-1140">
+      <!-- TOP NAV WITH LOGO -->          
+      <header class="margin-bottom">
+         <div class="line">
+            <nav>
+               <div class="top-nav">
+                  <p class="nav-text"></p>
+                  <a class="logo" href="index.php">            
+                  <!-- Fashion<span>Blog</span> -->
+                  <img src="img/cherry_inc.png" style="height:70px; width: auto;">
+                  </a>            
+                  <h1>Making Things Common</h1>
+                  <ul class="top-ul right">
+                     <li>            
+                        <a href="index.php">Blog</a>            
+                     </li>
+                     <li>            
+                        <a href="about.php">About</a>            
+                     </li>
+                     <li>            
+                        <a href="archive.php">Archive</a>            
+                     </li>
+                     <li>            
+                        <a href="contact.php">Contact</a>            
+                     </li>
+                     <div class="social right">	           
+                        <a target="_blank" href="https://www.facebook.com/cherryincnepal">
+                        <i class="icon-facebook_circle icon2x"></i>
+                        </a>          
+                        <a target="_blank" href="https://twitter.com/cherry_inc">
+                        <i class="icon-twitter_circle icon2x"></i>
+                        </a>          
+                     </div>
+                  </ul>
+               </div>
+            </nav>
+         </div>
+      </header>
